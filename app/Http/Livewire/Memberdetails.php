7371879ee4aca4e53a53a0d985e58f39f4b2e773
ziggsjimenez <juneljigjimenez@gmail.com>
2022-09-11 14:42:43 +0800

    update

--- app/Http/Livewire/Memberdetails.php
+++ app/Http/Livewire/Memberdetails.php
@@ -8,13 +8,31 @@ use Livewire\Component;
 class Memberdetails extends Component
 {
 
-    public $member_id,$employee; 
+    public $member_id, $employee, $btnSelected;
 
-    public function mount(){
+    public function mount()
+    {
         $this->employee = Employee::find($this->member_id);
     }
     public function render()
     {
         return view('livewire.memberdetails');
+    }
+
+    public function selectButton($num)
+    {
+
+
+        switch ($num) {
+            case 1:
+                $this->btnSelected = "Profile";
+                break;
+            case 2:
+                $this->btnSelected = "Accounts";
+                break;
+            case 3:
+                $this->btnSelected = "Loans";
+                break;
+        }
     }
 }
--- resources/views/livewire/memberdetails.blade.php
+++ resources/views/livewire/memberdetails.blade.php
@@ -1,35 +1,42 @@
 <div class="p-12">
-  
 
 
-    <div class="justify-center font-bold text-2xl">{{ $employee->fullname() }}</div>
+
+    <div class="justify-center font-bold text-2xl">{{ $employee->fullname() }} </div>
 
 
-    <div class="flex flex-row">
-        <div class="w-full bg-teal-200 m-5 p-5 rounded rounded-pill">
+    <div>
+        <button @if($btnSelected=="Profile") class="bg-blue-800 rounded font-bold text-xl p-2" @else class="bg-blue-500 rounded font-bold text-xl p-2" @endif wire:click="selectButton(1)" rounded p-3>Profile</button>
+
+        <button @if($btnSelected=="Accounts") class="bg-blue-800 rounded font-bold text-xl p-2" @else class="bg-blue-500 rounded font-bold text-xl p-2" @endif wire:click="selectButton(2)" rounded p-3>Accounts</button>
         
-            <h1 class="font-bold text-2xl justify-center">Profile</h1>
-            <table>
-                <tbody>
-                    <tr>
-                        <td class="font-bold p-2">Name: </td><td class="font-bold pl-5 p-2">{{ $employee->fullname() }}</td>
-                    </tr>
-                </tbody>
-            </table>
+        <button @if($btnSelected=="Loans") class="bg-blue-800 rounded font-bold text-xl p-2" @else class="bg-blue-500 rounded font-bold text-xl p-2" @endif wire:click="selectButton(3)" rounded p-3>Loans</button>
+    </div>
 
-            
-        
-        </div>
-        <div class="w-full bg-indigo-200 m-5 p-5 rounded rounded-pill">
-            <h1 class="font-bold text-2xl justify-center">Accounts</h1>   <i class="fa-solid fa-square-plus fa-2xl"></i> ADD
-        </div>
-        <div class="w-full bg-blue-200 m-5 p-5 rounded rounded-pill">
-            <h1 class="font-bold text-2xl justify-center">Loans</h1> <i class="fa-solid fa-square-plus fa-2xl"></i> ADD
-        </div>
-      </div>
+    <div class="border border-top">
 
+        @if($btnSelected=="Profile")
 
+        <h1 class="font-bold text-2xl justify-center">Profile</h1>
+        <table>
+            <tbody>
+                <tr>
+                    <td class="font-bold p-2">Name: </td>
+                    <td class="font-bold pl-5 p-2">{{ $employee->fullname() }}</td>
+                </tr>
+            </tbody>
+        </table>
 
+        @endif
 
+        @if($btnSelected=="Accounts")
+        <h1 class="font-bold text-2xl justify-center">Accounts</h1> <i class="fa-solid fa-square-plus fa-2xl"></i> ADD
+        @endif
+
+        @if($btnSelected=="Loans")
+        <h1 class="font-bold text-2xl justify-center">Loans</h1> <i class="fa-solid fa-square-plus fa-2xl"></i> ADD
+        @endif
+
+    </div>
 
 </div>
