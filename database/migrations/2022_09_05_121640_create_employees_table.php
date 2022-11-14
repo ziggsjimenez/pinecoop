e3cdc6d5b94e67ab699392665723bc47be8aa443
ziggsjimenez <juneljigjimenez@gmail.com>
2022-11-14 11:30:18 +0800

    update

--- database/migrations/2022_09_05_121640_create_employees_table.php
+++ database/migrations/2022_09_05_121640_create_employees_table.php
@@ -53,7 +53,7 @@ return new class extends Migration
             $table->date('dateofmembership')->nullable();
             $table->text('pwdid')->nullable();
             $table->boolean('deleted')->default(false);
-            $table->text('status')->default('Active');
+            $table->text('status')->nullable();
            
             $table->timestamps();
         });
